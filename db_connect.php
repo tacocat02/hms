ccad42a955886b16b14deff77794806a02503005
tacocat02 <120778236+tacocat02@users.noreply.github.com>
2024-06-11 14:27:50 +0545

    Delete db_connect.php

--- db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-//connect to database
-$conn=mysqli_connect("localhost","root","","hospital_management");//server/username/pass/db
-//check connection
-if(!$conn){
-    echo"Connection error:". mysqli_connect_error();
-}
